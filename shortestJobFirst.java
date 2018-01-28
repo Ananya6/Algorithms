ed2ebd19aa6a575f377cb2138a82a90aed387a8d
Ananya6 <ananyadhingra6@gmail.com>
2018-01-28 15:27:55 +0530

    Shortest Job First Non Preemptive

---
+++ shortestJobFirst.java
@@ -0,0 +1,76 @@
+
+import java.util.*;
+import java.util.Comparator;
+
+public class shortestJobFirst implements Comparable<shortestJobFirst> {
+    String pr = null;
+    int at;
+    int bt;
+    public int compareTo(shortestJobFirst o) {
+        return this.at-o.at;
+    }
+    public String toString() {
+        return String.format("(%s, %d,%d)", pr,at, bt);
+    }
+    shortestJobFirst(String m,int n,int o){
+        this.pr=m;
+        this.at=n;
+        this.bt=o;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        ArrayList<shortestJobFirst> main=new ArrayList<>();
+        //shortestJobFirst[] arr = new shortestJobFirst[n];
+        int i = 0;
+        for (; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            String pr="P"+(i+1);
+            int at = x;
+            int bt = y;
+           shortestJobFirst obj=new shortestJobFirst(pr,at,bt);
+            main.add(obj);
+        }
+
+        Collections.sort(main, new Comparator<shortestJobFirst>() {
+            @Override
+            public int compare(shortestJobFirst o1, shortestJobFirst o2) {
+                return o1.at-o2.at;
+            }
+        });
+        ArrayList<shortestJobFirst> sub=new ArrayList<>();
+        int totalTime=0;
+
+
+        i=0;
+        while(true){
+            //int j=0;
+            while(main.size()!=0&&(main.get(0).at<=totalTime)){
+                sub.add(main.remove(0));
+            }
+
+            Collections.sort(sub, new Comparator<shortestJobFirst>() {
+                @Override
+                public int compare(shortestJobFirst o1, shortestJobFirst o2) {
+                    return o1.bt-o2.bt;
+                }
+            });
+            totalTime+=sub.get(0).bt;
+           // main.remove(sub.get(0));
+            System.out.println(sub.remove(0).toString());
+            if(main.size()==0&&sub.size()==0)
+                break;
+
+        }
+
+
+    }
+
+
+
+}

f9e5c37221a5f8499572b1b3c207cf2ee033e8e0
Ananya6 <ananyadhingra6@gmail.com>
2018-03-18 16:32:36 +0530

    CPU scheduling algorithm

--- roundRobin.java
+++ roundRobin.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /**
  * Created on 2/8/2018.
  */
@@ -59,65 +59,3 @@ public class roundRobin {
 
     }
 }
-=======
-/**
- * Created on 2/8/2018.
- */
-import java.util.*;
-public class roundRobin {
-
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("No of processes:");
-        int n=sc.nextInt();
-        System.out.println("Enter quant");
-        int quant=sc.nextInt();
-        int[] burst=new int[n];
-        int[] finishTime=new int[n];
-        Arrays.fill(finishTime,-1);
-        ArrayList<Integer> order=new ArrayList<>();
-        int i=0;
-        int qmax=0,quotient=0,time=0;
-        for(;i<n;i++){
-            burst[i]=sc.nextInt();
-            quotient=burst[i]/quant;
-            if(quotient>qmax)
-                qmax=quotient;
-        }
-        int[] copyBurst=Arrays.copyOf(burst,n);
-        for(i=1;i<=qmax+1;i++){
-            for(int j=0;j<n;j++){
-                if(copyBurst[j]>=quant){
-                    copyBurst[j]-=quant;
-                    time+=quant;
-                    order.add(j+1);
-                }
-                else{
-                    if(copyBurst[j]!=0){
-                        time+=copyBurst[j];
-                        order.add(j+1);
-                        copyBurst[j]=0;
-                    }}
-
-                if(copyBurst[j]==0){
-                    if(finishTime[j]==-1)
-                        finishTime[j]=time;
-                }
-
-
-                }
-            }
-            float totalWait=0,totalTurn=0;
-
-        for(i=0;i<n;i++){
-            int wait=finishTime[i]-burst[i];
-            totalWait+=wait;
-            totalTurn+=finishTime[i];
-            System.out.println("P"+(i+1)+" Wait Time: "+wait+" TurnAround Time: "+finishTime[i]);
-        }
-        System.out.println("Order of processes"+order);
-        System.out.println("Average Waiting time: "+totalWait/n+" \nAverage Turnaround: "+totalTurn/n);
-
-    }
-}
->>>>>>> ec5df04e4a4e61180f6ea86e59891b6d9e59a06e
--- shortestJobFirst.java
+++ shortestJobFirst.java
@@ -23,81 +23,77 @@ public class shortestJobFirst implements Comparable<shortestJobFirst> {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        ArrayList<shortestJobFirst> main=new ArrayList<>();
+        ArrayList<shortestJobFirst> main = new ArrayList<>();
         //shortestJobFirst[] arr = new shortestJobFirst[n];
         int i = 0;
         for (; i < n; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
 
-            String pr="P"+(i+1);
+            String pr = "P" + (i + 1);
             int at = x;
             int bt = y;
-           shortestJobFirst obj=new shortestJobFirst(pr,at,bt);
+            shortestJobFirst obj = new shortestJobFirst(pr, at, bt);
             main.add(obj);
         }
 
         Collections.sort(main, new Comparator<shortestJobFirst>() {
             @Override
             public int compare(shortestJobFirst o1, shortestJobFirst o2) {
-                return o1.at-o2.at;
+                return o1.at - o2.at;
             }
         });
-        ArrayList<shortestJobFirst> sub=new ArrayList<>();
-        int totalTime=0;
-<<<<<<< HEAD
-        int waitTime=0;
-        int actualWaitTime=0;
+        ArrayList<shortestJobFirst> sub = new ArrayList<>();
+        int totalTime = 0;
+
+        int waitTime = 0;
+        int actualWaitTime = 0;
 
 
-
-        boolean first=true;
-        while(true){
-
-=======
+        boolean first = true;
+        while (true) {
 
 
-        i=0;
-        while(true){
-            //int j=0;
->>>>>>> ec5df04e4a4e61180f6ea86e59891b6d9e59a06e
-            while(main.size()!=0&&(main.get(0).at<=totalTime)){
-                sub.add(main.remove(0));
-            }
+            i = 0;
+            while (true) {
+                //int j=0;
 
-            Collections.sort(sub, new Comparator<shortestJobFirst>() {
-                @Override
-                public int compare(shortestJobFirst o1, shortestJobFirst o2) {
-                    return o1.bt-o2.bt;
+                while (main.size() != 0 && (main.get(0).at <= totalTime)) {
+                    sub.add(main.remove(0));
                 }
-            });
-<<<<<<< HEAD
-            shortestJobFirst element=sub.get(0);
-            totalTime+=element.bt;
-            if(first==true){
-                first=false;
 
-            }else{
-               actualWaitTime=waitTime-element.at;
+                Collections.sort(sub, new Comparator<shortestJobFirst>() {
+                    @Override
+                    public int compare(shortestJobFirst o1, shortestJobFirst o2) {
+                        return o1.bt - o2.bt;
+                    }
+                });
+
+                shortestJobFirst element = sub.get(0);
+                totalTime += element.bt;
+                if (first == true) {
+                    first = false;
+
+                } else {
+                    actualWaitTime = waitTime - element.at;
+                }
+                // main.remove(sub.get(0));
+                System.out.print(sub.remove(0).toString());
+                System.out.println("  Waiting time:" + actualWaitTime + "  TurnaroundTime:" + (actualWaitTime + element.bt));
+                waitTime += element.bt;
+
+                totalTime += sub.get(0).bt;
+                // main.remove(sub.get(0));
+                System.out.println(sub.remove(0).toString());
+
+                if (main.size() == 0 && sub.size() == 0)
+                    break;
+
             }
-           // main.remove(sub.get(0));
-            System.out.print(sub.remove(0).toString());
-            System.out.println("  Waiting time:"+actualWaitTime+"  TurnaroundTime:"+(actualWaitTime+element.bt));
-            waitTime+=element.bt;
 
-=======
-            totalTime+=sub.get(0).bt;
-           // main.remove(sub.get(0));
-            System.out.println(sub.remove(0).toString());
->>>>>>> ec5df04e4a4e61180f6ea86e59891b6d9e59a06e
-            if(main.size()==0&&sub.size()==0)
-                break;
 
         }
 
-
     }
-
-
 
 }
